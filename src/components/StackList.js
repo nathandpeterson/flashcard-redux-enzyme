78a0ef567c39e6b541064fcdb9668635be239c26
Nathan Daniel Peterson <natperson@gmail.com>
2018-03-09 13:01:31 -0800

    add redux to stacklist component

--- src/components/StackList.js
+++ src/components/StackList.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import stacks from '../data/stacks.json'
+import { setStack } from '../actions'
 
 class StackList extends Component {
-    render(){
-        return <div>{stacks.map(stack => {
-            return <Link key={stack.id} to='/stack'>
-                        <h4 >{stack.title}</h4>
-                    </Link>
-        })}</div>
+    render(){        
+        return (    
+            <div>{stacks.map(stack => {
+                return <Link    key={stack.id} 
+                                to='/stack'
+                                onClick={() => this.props.setStack(stack)}>
+                            <h4 >{stack.title}</h4>
+                        </Link>
+                                        })}
+            </div>
+        ) 
     }
 }
 
-export default StackList
+export default connect(null, { setStack })(StackList)
